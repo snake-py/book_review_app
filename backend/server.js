b2d901ddbc159a394745f352621869151ca64d8b
snake-py <vabischannel@gmail.com>
2020-09-14 14:36:52 +0200

    Basic Project Set Up

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
