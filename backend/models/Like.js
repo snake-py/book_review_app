eebbe4031750fb0babbb86d90fce334a009bbdd8
snake-py <vabischannel@gmail.com>
2020-09-17 13:09:18 +0200

    Renaming and enable out commeted code

--- backend/models/Like.js
+++ backend/models/Like.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const likeSchema = new Schema(
   {
     user_id: { type: Schema.Types.ObjectId, ref: 'User' },
-    //book_id: {type: Schema.Types.ObjectId, ref: 'Book'},
+    book_id: {type: Schema.Types.ObjectId, ref: 'Book'},
   },
   { timestamps: true }
 );
