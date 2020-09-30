10951aebb91faafb320582f515d6142047cb689b
snake-py <vabischannel@gmail.com>
2020-10-01 00:53:51 +0200

    Frontend Detail Page Style Suggestion

--- frontend/src/components/elements/BookDetail.js
+++ frontend/src/components/elements/BookDetail.js
@@ -16,44 +16,52 @@ class BookDetail extends Component {
   render() {
     return (
       <div>
-          <div className="container bookdetail">
-              <div className="row">
-                  <div className="col-2 image">
-                      <img src={this.props.book.thumbnail} alt={'picture of ' + this.props.book.title} />
-                  </div>
-                  <div className="col-10">
-                    <div className="row">
-                        <div className="col small_details">
-                              <h2 className="card-title">{this.props.book.title}</h2>
-                              <p className="card-text">Author: {this.props.book.author}</p>
-                              <p className="card-text">
-                                    <small className="text-muted">ISBN: {this.props.book.isbn}</small>
-                              </p>
-                        </div>
-                        <div className="col-3 rate">
-                          <div className="row">
-                              <div className="col-4">Rate </div>
-                              <div className="col">{this.props.book.rating}</div>
-                          </div>
-                          <div className ="row heart_detail">
-                              <div className="col-4"><i onClick={this.heartClickHandle} className="heart fas fa-heart"></i></div>
-                              <div className="col heart_sum">{this.props.book.likeSum}</div>
-                          </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                          <p className='text-justify text-left details'>
-                                  {this.props.book.description}
-                          </p>
-                        </div>
-                    </div>
-                  </div>
+        <div className="bookdetail">
+          <div className="row">
+            <div className="col-4 image">
+              <div style={imgStyle} >
+                <img className="img-fluid" src={this.props.book.thumbnail} alt={'picture of ' + this.props.book.title} />
               </div>
+            </div>
+            <div className="col-8">
+              <div className="row">
+                <div className="col small_details">
+                  <h2 className="card-title">{this.props.book.title}</h2>
+                  <p className="card-text">Author: {this.props.book.author}</p>
+                  <p className="card-text">
+                    <small className="text-muted">ISBN: {this.props.book.isbn}</small>
+                  </p>
+                </div>
+                <div className="col-3 rate">
+                  <div className="row">
+                    <div className="col-4">Rate </div>
+                    <div className="col">{this.props.book.rating}</div>
+                  </div>
+                  <div className="row heart_detail">
+                    <div className="col-4">
+                      <i onClick={this.heartClickHandle} className="heart fas fa-heart"></i>
+                    </div>
+                    <div className="col heart_sum">{this.props.book.likeSum}</div>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col">
+                  <p className="text-justify text-left details">{this.props.book.description}</p>
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
       </div>
     );
   }
 }
 
 export default withRouter(BookDetail);
+
+const imgStyle = {
+  height: '250px',
+  width: '250px',
+  overflow: 'hidden',
+};
--- frontend/src/components/pages/Detail.js
+++ frontend/src/components/pages/Detail.js
@@ -3,40 +3,43 @@ import BookDetail from '../elements/BookDetail';
 import SearchBar from '../elements/SearchBar';
 import ReviewCard from '../elements/ReviewCard';
 
-
-export default class Detail extends Component{
-    book = {
-      isbn: '564-135468464',
-      title: 'Book 1',
-      author: 'Author 1',
-      thumbnail: 'https://picsum.photos/150/150?random=1',
-      likeSum: 34,
-      description: 'vhejbvnjebvjhebvjhebvjhebvjhebvjebvjebvjfevjhebvjhebvjhebvjhebvjebvjebvjfevjhebvjhebvjhebvjhebvjebvjebvjfevjhebvjhebvjhebvjhebvjebvjebvjfevjhebvjhebvjhebvjhebvjebvjebvjfevjhebvjhebvjhebvjhebvjebvjebvjfevjhebvjhebvjhebvjhebvjebvjebvjfevjhebvjhebvjhebvjhebvjebvjebvjfevjhebvjhebvjhebvjhebvjebvjebvjfevjhebvjhebvjhebvjhebvjebvjebvjfevjhebvjhebvjhebvjhebvjebvjebvjfebjfbvjdfhvddfkjnvkdfnv dfvn dfvndhvdhbvdhfbvdhfbvdhfbvdhfbvdhfvbdhfvbdhfvbdhvbdhvbdhbvdfhvbdfhvdfvjdfnvjdfhvdjfvdjvhbdjvhbdfjvbdvdv',
-      rating: 8.5,
-    }
-    review = {
-        title: 'Great Book',
-        rating: 8,
-        text: 'jkdsnkjsndkvjnskvnskdjvnsdkjvnsdkjvnskjdvnskdjvnsdjvnskvjnskdvnksjdvnskjdvsjvnsdnkvskndskjdvnsdjnksjdvnskjdvsjvnsdnkvskndskjdvnsdjnksjdvnskjdvsjvnsdnkvskndskjdvnsdjnksjdvnskjdvsjvnsdnkvskndskjdvnsdjnksjdvnskjdvsjvnsdnkvskndskjdvnsdjkvnsdvnsknvskjdnvskndv',
-        user: 'User1',
-        date: '23/07/2001',
-    }
-    render() {
-        return (
-          <React.Fragment>
-            <header>
-              <div className="search_bar_detail">
-                <SearchBar/>
-              </div>
-            </header>
-            <section className = "detailPage">
-                <BookDetail book={this.book}/>
-                <h5 className="review_section">Reviews</h5>
-                <div className = "reviewCards">
-                    <ReviewCard review={this.review}/>
-                </div>
-            </section>
-          </React.Fragment>
-        );
-      }
+export default class Detail extends Component {
+  book = {
+    isbn: '564-135468464',
+    title: 'Book 1',
+    author: 'Author 1',
+    thumbnail: 'https://picsum.photos/300/300?random=1',
+    likeSum: 34,
+    description:
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.',
+    rating: 8.5,
+  };
+  review = {
+    title: 'Great Book',
+    rating: 8,
+    text:
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    user: 'User1',
+    date: '23/07/2001',
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <header>
+          <div className="container mt-5">
+            <div className="search_bar_detail">
+              <SearchBar />
+            </div>
+          </div>
+        </header>
+        <section className="container mt-5">
+          <BookDetail book={this.book} />
+          <h5 className="review_section">Reviews</h5>
+          <div className="reviewCards">
+            <ReviewCard review={this.review} />
+          </div>
+        </section>
+      </React.Fragment>
+    );
+  }
 }
